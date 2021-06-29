0355f265619f47a9c4b1a101377e6364af3e2c00
sahar227 <sahar22798@gmail.com>
2021-06-29 22:02:52 +0300

    Made sure audio gets played

--- src/components/Phase1QuestionsManager/Phase1QuestionsManager.jsx
+++ src/components/Phase1QuestionsManager/Phase1QuestionsManager.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
+const soundClient = require("soundoftext-js");
 
-// TODO: Play sound
-// TODO 2:style this component
+// TODO: style this component
 
 const showLetterTimeInMS = 1000;
 
@@ -13,30 +13,32 @@ const LetterPhase = ({ letter }) => {
   );
 };
 
-const QuestionPhase = ({ question, setNextQuestion, addReportForQuestion }) => {
-  const startTime = performance.now();
-  const { word, answer, imageURL, letter } = question;
-  const giveAnswer = (userAnswer) => {
-    const report = {
-      word,
-      letter,
-      answer,
-      userAnswer,
-      secondsToAnser: (performance.now() - startTime) / 1000,
+const QuestionPhase = React.memo(
+  ({ question, setNextQuestion, addReportForQuestion }) => {
+    const startTime = performance.now();
+    const { word, answer, imageURL, letter } = question;
+    const giveAnswer = (userAnswer) => {
+      const report = {
+        word,
+        letter,
+        answer,
+        userAnswer,
+        secondsToAnser: (performance.now() - startTime) / 1000,
+      };
+      addReportForQuestion(report);
+      setNextQuestion();
     };
-    addReportForQuestion(report);
-    setNextQuestion();
-  };
-  return (
-    <div>
-      <h1>{word}</h1>
-      <h1>{answer.toString()}</h1>
-      <img src={imageURL} />
-      <button onClick={() => giveAnswer(true)}>True</button>
-      <button onClick={() => giveAnswer(false)}>False</button>
-    </div>
-  );
-};
+    return (
+      <div>
+        <h1>{word}</h1>
+        <h1>{answer.toString()}</h1>
+        <img src={imageURL} />
+        <button onClick={() => giveAnswer(true)}>True</button>
+        <button onClick={() => giveAnswer(false)}>False</button>
+      </div>
+    );
+  }
+);
 
 export default function Phase1QuestionsManager({
   questions,
@@ -56,6 +58,16 @@ export default function Phase1QuestionsManager({
     setTimeout(() => {
       setShowingLetter(false);
     }, showLetterTimeInMS);
+  }, [currentQuestionIndex]);
+
+  // get soundID
+  // TODO: can possibly reduce time it takes, by getting all the sound urls beforehand, and maybe I can also load the mp3s themselves earlier
+  useEffect(() => {
+    soundClient.sounds
+      .create({ text: currentQuestion.word, voice: "cy" })
+      .then((soundURL) => {
+        new Audio(soundURL).play();
+      });
   }, [currentQuestionIndex]);
 
   return (
