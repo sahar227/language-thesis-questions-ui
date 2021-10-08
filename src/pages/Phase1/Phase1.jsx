104c6a88bb79ff4f4d4146c40eef99b2c5c2a8b4
sahar227 <sahar22798@gmail.com>
2021-10-08 18:38:39 +0300

    Added instructions

--- src/pages/Phase1/Phase1.jsx
+++ src/pages/Phase1/Phase1.jsx
@@ -4,6 +4,21 @@ import practiceQuestionsPhase1 from "../../staticData/practiceQuestionsPhase1";
 
 export default function Phase1({ questions, nextScreen }) {
   const [isPractice, setIsPractice] = useState(true);
+  const [isStarted, setIsStarted] = useState(false);
+
+  if (!isStarted) {
+    return (
+      <div>
+        <p>למטלה זו תוצג בפניכם אות ולאחר מכן מילה בשפה זרה.</p>
+        <p>
+          אם האות מופיעה במילה תציינו כן ואם האות לא מופיעה במילה תציינו לא.
+        </p>
+        <p>עלייך להשיב בצורה המדויקת והמהירה ביותר.</p>
+        <button onClick={() => setIsStarted(true)}>התחל</button>
+      </div>
+    );
+  }
+
   return (
     <div>
       {isPractice && (
--- src/pages/Phase2/Phase2.jsx
+++ src/pages/Phase2/Phase2.jsx
@@ -4,6 +4,26 @@ import practiceQuestionsPhase2 from "../../staticData/practiceQuestionsPhase2";
 
 export default function Phase2({ questions, nextScreen }) {
   const [isPractice, setIsPractice] = useState(true);
+  const [isStarted, setIsStarted] = useState(false);
+
+  if (!isStarted) {
+    return (
+      <div>
+        <p>תודה על שיתוף הפעולה!</p>
+        <p>
+          כעת תאזינו למילים נוספות בשפה זרה. עבור כל מילה שתשמעו, יופיע במסך
+          תרגום מסוים.
+        </p>
+        <p>
+          אם התרגום נכון עליכם להשיב "כן" ואם התרגום שגוי עליכם להשיב "לא".{" "}
+        </p>
+        <br />
+        <p>אחרי כל תשובה שתשיבו המחשב יאמר האם נכונה או לא נכונה.</p>
+        <p> עליכם להשתמש במידע זה כדי ללמוד כמה שיותר מילים.</p>
+        <button onClick={() => setIsStarted(true)}>התחל</button>
+      </div>
+    );
+  }
 
   return (
     <div>
