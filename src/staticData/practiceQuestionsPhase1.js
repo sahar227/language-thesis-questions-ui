0f49bdcd1c0238d877b83a02ae18fcfcf6099ab2
sahar227 <sahar22798@gmail.com>
2024-06-17 19:38:41 +0300

    Updated example question images

--- src/staticData/practiceQuestionsPhase1.js
+++ src/staticData/practiceQuestionsPhase1.js
@@ -4,21 +4,21 @@ const practiceQuestionsPhase1 = [
     answer: true,
     letter: "g",
     imageURL:
-      "https://i.pinimg.com/564x/3a/88/07/3a88075b98d5bf96b791d7498a94b5ab.jpg",
+      "https://i.ibb.co/W5Z8jKp/ant.jpg",
   },
   {
     word: "wen",
     answer: false,
     letter: "b",
     imageURL:
-      "https://helloartsy.com/wp-content/uploads/kids/farming/how-to-draw-a-barn/how-to-draw-a-barn-step-9.jpg",
+      "https://i.ibb.co/qMysYhq/barn.webp",
   },
   {
     word: "gwely",
     answer: true,
     letter: "y",
     imageURL:
-      "https://i.pinimg.com/originals/ac/3d/32/ac3d32bb14afe378d758fab4010a6fe5.png",
+      "https://i.ibb.co/0hpRzqs/bed.jpg",
   },
 ];
 
